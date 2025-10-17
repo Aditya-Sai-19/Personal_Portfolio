04331d061db64c05b1c4825028afc5a1ce893117
K Aditya Sai <adityasai2004@gmail.com>
2025-10-17 13:19:39 +0530

    Change certification color from blue to yellow
    Updated certification color scheme to reflect new branding.

--- components/Certifications.tsx
+++ components/Certifications.tsx
@@ -11,7 +11,7 @@ export default function Certifications() {
   platform: 'Oracle',
   date: 'October 2025',
   category: 'AI/Cloud',
-  color: 'from-blue-500 to-cyan-400',
+  color: 'from-stellar-yellow to-orange-500',
   description: 'Certification demonstrating expertise in Oracle Cloud Infrastructure and Generative AI services',
   link: 'https://catalog-education.oracle.com/pls/certview/sharebadge?id=0BC5481C6D8F23D8372FE1FDE216279DEE629D205BFAF74BBBF0AA1A7E848A93'
 },
